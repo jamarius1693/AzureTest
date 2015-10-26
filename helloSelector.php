00aa1b97dae2fd05a092f8d9fcc851203afe4d9d
marius <m.i.petrovan@rgu.ac.uk>
2015-10-26 15:45:54 +0000

    helloselector

--- helloSelector.php
+++ helloSelector.php
@@ -3,10 +3,3 @@
     $type = $GET_["type"];
     echo "The query is " . $cat .  "and the type is " . $type;
 ?>
-
-/**
- * Created by PhpStorm.
- * User: 1208470
- * Date: 26/10/2015
- * Time: 15:35
- */
