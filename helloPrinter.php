bfbad0e3008a9a0a354b0eaaf68c6d06aedeb826
marius <m.i.petrovan@rgu.ac.uk>
2015-10-26 15:41:41 +0000

    printers

---
+++ helloPrinter.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1208470
+ * Date: 26/10/2015
+ * Time: 15:35
+ */
---
+++ helloSelector.php
@@ -0,0 +1,12 @@
+<?php
+    $query = $_GET["query"];
+    $type = $GET_["type"];
+    echo "The query is " . $cat .  "and the type is " . $type;
+?>
+
+/**
+ * Created by PhpStorm.
+ * User: 1208470
+ * Date: 26/10/2015
+ * Time: 15:35
+ */
