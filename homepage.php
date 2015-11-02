352abaefc173cf2f9a399f3cb27fa81c7e04f182
marius <m.i.petrovan@rgu.ac.uk>
2015-11-02 14:28:01 +0000

    main_login

---
+++ homepage.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1208470
+ * Date: 02/11/2015
+ * Time: 14:00
+ */
+var $access_level = $_COOKIE['access_level'];
+function displayAccessLevelInformation($access_level) {
+
+    if ($access_level == "standarduser") {
+        echo "<p>You are currently logged in as a standard user</p>";
+    }
+    elseif ($access_level == "root") {
+        echo "<p>You are currently logged in as a root user</p>";
+        echo "<p>You now have access to additional administrative features</p>";
+    }
+}
+?>
