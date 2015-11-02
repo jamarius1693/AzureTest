6fb569ea676379df2a0d4fc2ee68354a4c54e53f
marius <m.i.petrovan@rgu.ac.uk>
2015-11-02 15:28:05 +0000

    homepage redirect

--- homepage.php
+++ homepage.php
@@ -7,14 +7,15 @@
  */
 echo "<p>hello</p>";
 $access_level = $_COOKIE["access_level"];
-function displayAccessLevelInformation() {
+function displayAccessLevelInformation($access_level) {
 
-    if ($_COOKIE["access_level"] == "standarduser") {
+    if ($access_level == "standarduser") {
         echo "<p>You are currently logged in as a standard user</p>";
     }
-    elseif ($_COOKIE["access_level"] == "root") {
+    elseif ($access_level == "root") {
         echo "<p>You are currently logged in as a root user</p>";
         echo "<p>You now have access to additional administrative features</p>";
     }
 }
+displayAccessLevelInformation($access_level);
 ?>
