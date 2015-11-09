2732d4137a4d1c468a709072844ccf0b71bad294
marius <m.i.petrovan@rgu.ac.uk>
2015-11-09 15:38:28 +0000

    database connection + index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1208470
+ * Date: 09/11/2015
+ * Time: 15:27
+ */
+$db = new mysqli(
+    "eu-cdbr-azure-west-c.cloudapp.net",
+    "ba3185d0da41b9",
+    "814afc85",
+    "firsttry1693" );
+
+// test our connection
+if ($db->connect_errno) {
+    die ('Connection Failed :'.$db->connect_error );
+    echo 'wololo';
+}
+
+$section = $_GET['section'];
+echo $section;
+?>
